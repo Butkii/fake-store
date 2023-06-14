7b69c462d69520f8557cac8fe79a3a525642c55b
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2023-06-14 18:09:47 +0400

    Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,6 +5,7 @@ import {
     Grid,
     SimpleGrid,
     Button,
+    Spacer,
 } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -32,23 +33,27 @@ function Categories({categories}) {
     }
 
     return <Box paddingX={20} paddingTop={20}>
-        <Heading fontWeight={700} size="lg" color="text.title" textAlign="center">
-            Our Premium Collection
-        </Heading>
-        <Flex  paddingY={6} justifyContent="center">
-            {categories.map(item => {
-                return <Heading backgroundColor="transparent" mx={4} fontSize="16px" fontWeight="700" color="text.body2" as={Button} onClick={() => {
-                    filterProducts(item);
-                }}>
-                {item}
-                </Heading>
-            })}
+        <Flex direction="column" justifyContent="center">
+            <Heading fontWeight={700} size="lg" color="text.title" textAlign="center">
+                Our Premium Collection
+            </Heading>
+            <Flex  paddingY={6} alignItems="center">
+                {categories.map(item => {
+                    return <Heading backgroundColor="transparent" mx={4} fontSize="16px" fontWeight="700" color="text.body2" as={Button} onClick={() => {
+                        filterProducts(item);
+                    }}>
+                    {item}
+                    </Heading>
+                })}
+            </Flex>
+            <SimpleGrid columns={3} spacing="10px" minWidth="100px">
+                {products.map(item => {
+                    return <StoreItem title={item.title} category={item.category} image={item.image} />
+                })}
+            </SimpleGrid>
+            <Button mt={4} width={36} alignSelf="center" backgroundColor="secondary.500" color="base.white">Find Out More</Button>
         </Flex>
-        <SimpleGrid columns={3} spacing="10px" minWidth="100px">
-            {products.map(item => {
-                return <StoreItem title={item.title} category={item.category} image={item.image} />
-            })}
-        </SimpleGrid>
+        
     </Box>
 }
 
