1f1e85331e4a81168803ff3e8fd090a43f9a30f1
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2023-06-14 21:26:11 +0400

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import {
+    Box,
+    Image,
+    Text,
+} from '@chakra-ui/react';
+
+function Footer() {
+    return <Box>
+        <Image width="100%" src="/img/Footer.svg"></Image>
+    </Box>
+}
+
+export default Footer;
